8faceae6df890ab1bd5267299ecdfcf305341652
raven1911 <vuduclam1911@gmail.com>
2024-11-04 23:21:44 +0700

    done

--- Lab3_vxl_f1/Core/Src/fsm_manual.c
+++ Lab3_vxl_f1/Core/Src/fsm_manual.c
@@ -7,7 +7,20 @@
 
 #include "fsm_manual.h"
 
-
+uint8_t manual_led_0 = 0;
+uint8_t manual_led_1 = 0;
 void fsm_manual(){
+	if(isButtonPressed(1)){
+		manual_led_0++;
+		if(manual_led_0>2) manual_led_0 = 0;
+	}
+	if(isButtonLongPressed(1)){
+		manual_led_1++;
+		if(manual_led_1>2) manual_led_1 = 0;
+	}
+
+	manual_led(manual_led_0, manual_led_1);
+	updateClockBuffer(0, 0);
+	scan_led();
 
 }
